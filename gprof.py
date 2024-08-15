b2e874c46b9ccc65da3955668067f47779384261
Daniel Kulichkov <danchick03@gmail.com>
2024-08-15 12:09:12 +0300

    feat: add change git profile feature

---
+++ gprof.py
@@ -0,0 +1,50 @@
+import sys
+import os
+from dataclasses import dataclass
+import tomllib # ! requires python 3.11
+
+
+@dataclass
+class GitPofile:
+  email: str
+  name: str
+
+
+def change_profile(profile: GitPofile):
+  pass
+
+
+def get_profile_config(profile_name: str):
+   pass
+
+
+def main():
+  # parse args
+  args = sys.argv[1:]
+  if len(args) != 1:
+    print("Usage:\n\tgprof [profile_name]")
+    return
+  
+  profile_name = args[0]
+
+  # load specific profile
+  profiles_config_path = os.path.expanduser("~/.config/gprof/profiles")
+  with open(profiles_config_path, "rb") as file:
+    profiles_config = tomllib.load(file)
+    certain_profile_config = profiles_config.get(profile_name)
+    if certain_profile_config is None:
+      print(f"Cannot find the profile with the name \"{profile_name}\"")
+      return
+    profile = GitPofile(
+      certain_profile_config.get("email"), 
+      certain_profile_config.get("name")
+    )
+
+  os.system(f"git config --global --replace-all user.name \"{profile.name}\"")
+  os.system(f"git config --global --replace-all user.email \"{profile.email}\"")
+  print(f"\t> Changed your git profile to {profile.email}")
+  
+
+
+if __name__ == "__main__":
+    main()
